7a0ff6c00d3aa3e3530113cd9b1d244d8f03d150
cnttkhs <cnt.tkhs@gmail.com>
2018-03-25 16:36:47 +0900

    Use reloader

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require 'sinatra/base'
+require 'sinatra/reloader'
 
 require './modules/fizzbuzz'
 
 class App < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   # FizzBuzz
   get '/fizzbuzz' do
     @results = FizzBuzz.results
